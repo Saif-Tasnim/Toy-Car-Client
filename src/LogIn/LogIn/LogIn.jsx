7e7f6669b51cce479c53766547bd1ed7ec2f39b3
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-06-23 15:13:42 +0600

    basic form dynamicity uploaded

--- src/LogIn/LogIn/LogIn.jsx
+++ src/LogIn/LogIn/LogIn.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import img from '../../../src/assets/images.png'
 
 const LogIn = () => {
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(email, password);
+        // form.reset();
+    }
+
     return (
         <div className="min-h-screen bg-base-200 w-[1140px] mx-auto rounded-lg mt-20 p-8">
 
@@ -13,21 +25,21 @@ const LogIn = () => {
                     <p className="py-6 mt-3 text-red-600 font-bold">Unlock the door to a world of imagination and adventure</p>
                 </div>
 
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card flex-shrink-0 w-full max-w-sm h-[450px] shadow-2xl bg-base-100">
                     <div className="card-body">
 
-                        <form>
+                        <form onSubmit={handleLogin}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input type="email" placeholder="email" className="input input-bordered" name="email" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <input type="password" placeholder="password" className="input input-bordered" name='password' required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
@@ -36,6 +48,10 @@ const LogIn = () => {
                                 <button className="btn btn-primary">Login</button>
                             </div>
                         </form>
+                        <div className="divider">OR</div>
+                        <div className="form-control mt-2">
+                            <button className="btn btn-primary">Sign In With Google</button>
+                        </div>
 
                     </div>
                 </div>
