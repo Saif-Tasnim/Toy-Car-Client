8dbfec30c9e3b7e154566b06c0280e6543252b6c
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-06-23 12:45:53 +0600

    toy sports car has done

--- src/Home/ShopCategory/Category/ToyCar/ToyCar.jsx
+++ src/Home/ShopCategory/Category/ToyCar/ToyCar.jsx
@@ -7,34 +7,35 @@ const ToyCar = () => {
     useEffect(() => {
         fetch('http://localhost:5000/carData?category=Toy%20Sports%20Car')
             .then(res => res.json())
-            .then(data => console.log(data));
+            .then(data => setCar(data));
     }, [])
 
-    return (
-        <div className='mt-8'>
-            {/* card -1  */}
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
+    if (car.length > 2) {
+        console.log(car.slice(0, 2));
+        console.log("new Array ize is ", car.length);
+    }
 
-            {/* card-2 */}
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+    else {
+        console.log("size is perfect");
+    }
+
+    return (
+        <div className='mt-9 grid grid-cols-1 md:grid-cols-2 ml-20'>
+            {
+                car.map(c => <>
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                        <figure><img src={c.picture} alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title"> {c.name} </h2>
+                            <p>Price : {c.price} </p>
+                            <p>Ratings : {c.rating} </p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">View Details Button</button>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </>)
+            }
 
         </div>
     );
